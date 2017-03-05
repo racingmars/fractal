c7dffcb6119e173361f5843dfbf8fc386b2fed9a
Matthew R. Wilson <mwilson@mattwilson.org>
2017-03-04 22:55:47 -0800

    Add histogram color method

--- main.go
+++ main.go
@@ -10,37 +10,80 @@ import (
 	"math/cmplx"
 	"os"
 	"runtime"
+	"sync"
 	"time"
 )
 
-const iterations = 256
+const iterations = 250
 const width = 1600
+const useHistogramMethod bool = false
 
 func main() {
 	heighttmp := width / 1.75
 	height := int(heighttmp)
+	histo := make([]int, iterations)
+	histodata := make(chan int, 100)
+	fractal := make([][]int, width)
+	for i := range fractal {
+		fractal[i] = make([]int, height)
+	}
+
 	img := image.NewGray(image.Rect(0, 0, width, height))
 
 	workers := runtime.NumCPU()
 	runtime.GOMAXPROCS(workers)
 
 	rows := make(chan int, workers)
-	done := make(chan bool, workers)
+	var workerWg, histoWg sync.WaitGroup
+
+	histoWg.Add(1)
+	go buildHisto(histo, histodata, &histoWg)
 
 	start := time.Now().UTC().UnixNano()
 	for w := 0; w < workers; w++ {
-		go worker(img, width, height, rows, done)
+		workerWg.Add(1)
+		go worker(fractal, width, height, rows, histodata, &workerWg)
 	}
 	for y := 0; y < height; y++ {
 		rows <- y
 	}
 	close(rows)
-	for w := 0; w < workers; w++ {
-		<-done
-	}
+	workerWg.Wait()
 	stop := time.Now().UTC().UnixNano()
 	runtime := stop - start
 	fmt.Println("Time to run:", float32(runtime)/1000000000)
+
+	close(histodata)
+	histoWg.Wait()
+
+	total := 0
+	for _, count := range histo {
+		total += count
+	}
+
+	colors := make([]float64, iterations)
+	for i := 0; i < iterations; i++ {
+		color := 0.0
+		for j := 0; j < i; j++ {
+			color += float64(histo[j]) / float64(total)
+		}
+		colors[i] = color
+	}
+
+	for x := range fractal {
+		for y := range fractal[x] {
+			if fractal[x][y] < 0 {
+				img.Set(x, y, color.Black)
+			} else {
+				if !useHistogramMethod {
+					gray := float64(fractal[x][y]) / float64(iterations) * float64(math.MaxUint8)
+					img.SetGray(x, y, color.Gray{uint8(math.MaxUint8 - gray)})
+				} else {
+					img.SetGray(x, y, color.Gray{uint8(colors[fractal[x][y]] * math.MaxUint8)})
+				}
+			}
+		}
+	}
 
 	f, err := os.Create("image.png")
 	if err != nil {
@@ -55,21 +98,21 @@ func main() {
 	f.Close()
 }
 
-func worker(img *image.Gray, width, height int, row chan int, done chan bool) {
+func worker(img [][]int, width, height int, row, histodata chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for y := range row {
 		for x := 0; x < width; x++ {
 			r := float64(x)/(float64(width)-1)*3.5 - 2.5
 			i := float64(y)/(float64(height)-1)*2 - 1
 			in, iter := inSet(complex(r, i))
 			if in {
-				img.Set(x, y, color.Black)
+				img[x][y] = -1
 			} else {
-				gray := float64(iter) / float64(iterations) * float64(math.MaxUint8)
-				img.SetGray(x, y, color.Gray{uint8(math.MaxUint8 - gray)})
+				img[x][y] = iter
+				histodata <- iter
 			}
 		}
 	}
-	done <- true
 }
 
 func inSet(c complex128) (bool, int) {
@@ -81,4 +124,11 @@ func inSet(c complex128) (bool, int) {
 		}
 	}
 	return true, 0
+}
+
+func buildHisto(histo []int, data chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := range data {
+		histo[i]++
+	}
 }
